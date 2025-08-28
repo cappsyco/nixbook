a695af4b1ea71ed852c5be561b2d0a0f58e305f3
Jonathan Capps <cappsy@gmail.com>
2025-08-28 20:56:18 +0100

    adding cosmic install and configs

---
+++ install_cosmic.sh
@@ -0,0 +1,33 @@
+echo "This will delete ALL local files and convert this machine to a Nixbook!";
+read -p "Do you want to continue? (y/n): " answer
+
+if [[ "$answer" =~ ^[Yy]$ ]]; then
+  echo "Installing NixBook..."
+
+  # Set up local files
+  rm -rf ~/
+  mkdir ~/Desktop
+  mkdir ~/Documents
+  mkdir ~/Downloads
+  mkdir ~/Pictures
+  mkdir ~/.local
+  mkdir ~/.local/share
+  cp -R /etc/nixbook/config/applications ~/.local/share/applications
+
+  # The rest of the install should be hands off
+  # Add Nixbook config and rebuild
+  sudo sed -i '/hardware-configuration\.nix/a\      /etc/nixbook/base.nix' /etc/nixos/configuration.nix
+  
+  # Set up flathub repos while we have sudo
+  nix-shell -p flatpak --run 'sudo flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo'
+  nix-shell -p flatpak --run 'sudo flatpak remote-add --if-not-exists --user cosmic https://apt.pop-os.org/cosmic/cosmic.flatpakrepo'
+
+  sudo nixos-rebuild switch
+
+  # Add flathub and some apps
+  flatpak install flathub org.mozilla.firefox -y
+  
+  reboot
+else
+  echo "Nixbook Install Cancelled!"
+fi
